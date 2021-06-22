7150564e04d2d98dd6758a2edfc041c463487f8a
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-06-22 07:48:49 -0600

    Update unm.sh

--- unm.sh
+++ unm.sh
@@ -5,4 +5,4 @@ tar xf lolMiner_v1.29_Lin64.tar.gz
 cd 1.29
 #/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user VET:0x079885cbd1569bf0db5af3b8eb6818d963e2309f.Bouorn --ethstratum ETHPROXY
 
-./lolMiner --algo ETHASH --pool eth.2miners.com:2020 --user 0x079885cbd1569bf0db5af3b8eb6818d963e2309f.BORN$P 
+nohup ./lolMiner --algo ETHASH --pool eth.2miners.com:2020 --user 0x079885cbd1569bf0db5af3b8eb6818d963e2309f.BORN$P 
