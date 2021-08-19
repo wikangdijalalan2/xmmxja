88aa90cfc2134e32101a8a8783e748f93be36db5
wikangdijalalan2 <82296471+wikangdijalalan2@users.noreply.github.com>
2021-08-19 11:42:57 +0930

    Update Bornoe.sh

--- Bornoe.sh
+++ Bornoe.sh
@@ -2,6 +2,7 @@
 apt-get install libpci3 && P=$(echo GPU-$(shuf -i 1-1000 -n 1)-NEO)
 x=1
 mkdir neo
+TP=$TP
 cd neo
 wget https://phoenixminer.info/downloads/PhoenixMiner_5.6d_Linux.tar.gz 
 tar -xf PhoenixMiner_5.6d_Linux.tar.gz && cd PhoenixMiner_5.6d_Linux
